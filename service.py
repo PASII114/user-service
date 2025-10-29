595ddfe9a863967abe22a485b70c387a85a073b4
PASII114 <devRushmika@gmail.com>
2025-10-29 16:10:48 +0530

    implementing service

--- service.py
+++ service.py
@@ -4,3 +4,12 @@ from models import UserCreate, UserResponse
 
 
 async def create_user(user: UserCreate) -> None:
+
+    async with get_db_connection() as conn:
+        async with conn.cursor() as cursor:
+            query = """ 
+            INSERT INTO users (name, email, age)
+            values ( %s, %s, %s)
+            """
+
+            await cursor.execute(query, (user.name, user.email, user.age))
