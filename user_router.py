2435b86b2b050860b4ab00f42b2f2e028e9a5be1
PASII114 <devRushmika@gmail.com>
2025-11-03 14:34:25 +0530

    bruh

--- user_router.py
+++ user_router.py
@@ -9,4 +9,9 @@ router = APIRouter(prefix="/users")
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserCreate):
+    existing_user = await service.get_user_by_email(user.email)
+
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User already exists")
+
     return await service.create_user(user)
