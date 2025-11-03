777dadcabb081d554a296fbed2a777f8eced616e
PASII114 <devRushmika@gmail.com>
2025-11-03 17:08:20 +0530

    k

--- service.py
+++ service.py
@@ -1,6 +1,7 @@
 from typing import Optional, List
 
 import aiomysql
+from dns.e164 import query
 from pydantic import EmailStr
 from starlette import status
 from starlette.exceptions import HTTPException
