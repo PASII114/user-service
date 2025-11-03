4e850d790e5ea70200f38e7717b9969ba9de2e8e
PASII114 <devRushmika@gmail.com>
2025-11-03 15:04:26 +0530

    delete_api

--- user_router.py
+++ user_router.py
@@ -23,3 +23,17 @@ async def create_user(user: UserCreate):
 @router.get("/", status_code=status.HTTP_200_OK)
 async def get_users(limit: int = 10, offset: int = 0) -> List[UserResponse]:
     return await service.get_all_users(limit, offset)
+
+@router.get("/{user_id}", status_code=status.HTTP_200_OK)
+async def get_user_by_id(user_id: int):
+    return await service.get_user_by_id(user_id)
+
+@router.delete("/{user_id}", status_code=status.HTTP_200_OK)
+async def delete_user_by_id(user_id: int):
+    existing_user = await service.get_user_by_id(user_id)
+    if not existing_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User Does Not Exists")
+
+    await service.delete_user_by_id(user_id)
+
+    return {"info" : "User Deleted Successfully", "user" : existing_user}
